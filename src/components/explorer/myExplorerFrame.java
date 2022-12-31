cb24436f1e662f694443327d646703adbe8937ee
ZainV4 <Zainv4@gmail.com>
2022-12-31 02:27:09 -0500

    JFE

--- src/components/explorer/myExplorerFrame.java
+++ src/components/explorer/myExplorerFrame.java
@@ -1,15 +1,25 @@
 package components.explorer;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 import java.awt.Color;
-import javax.swing.JFrame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.JOptionPane;
+import java.io.File;
+import java.awt.*;
 
 
 public class myExplorerFrame extends JFrame {
-    public myExplorerFrame () {
 
+    JList<myDirectory> list = new JList<>();
+    DefaultListModel<myDirectory> model = new DefaultListModel<>();
+    File[] drives = File.listRoots();
+
+    JLabel label = new JLabel();
+    JPanel panel = new JPanel();
+    JSplitPane splitPane = new JSplitPane();
+    public myExplorerFrame () {
         // using the Color class to create my own color
         Color myBackgroundColor = new Color(93, 137, 178);
 
@@ -26,12 +36,9 @@ public class myExplorerFrame extends JFrame {
             System.out.println(ex);
         }
 
-
-        // makes frame to the color of the variable myBackgroundColor
-        this.getContentPane().setBackground(myBackgroundColor);
-
         // do nothing if user tries to exit
         this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
         // event listener
         this.addWindowListener(new WindowAdapter() {
             //if user tries to exit ConfirmDialog will appear
@@ -44,8 +51,43 @@ public class myExplorerFrame extends JFrame {
                     System.exit(0);
                 }
             }
- 
         });
+        ///////////////////////////////////////////////////////////////////
+        list.setModel(model);
+        if (drives != null && drives.length > 0) {
+            for (File aDrive : drives) {
+                model.addElement(new myDirectory(aDrive));
+            }
+        }
+
+        list.getSelectionModel().addListSelectionListener(e -> {
+            myDirectory p = list.getSelectedValue();
+            String contents[] = p.getFile().list();
+            label.setText("Name: " + p.getFile());
+            panel.removeAll();
+            panel.updateUI();
+            for(int i=0; i<contents.length; i++) {
+                JButton btn =  new JButton(contents[i]);
+                btn.setFocusPainted(false);
+                btn.setMaximumSize(new Dimension(1870, 50));
+                btn.setMinimumSize(new Dimension(1870, 50));
+                panel.add(btn);
+            }
+        });
+
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(myBackgroundColor);
+        splitPane.setLeftComponent(new JScrollPane(list));
+        splitPane.setDividerLocation(220);
+        JScrollPane scrollPane = new JScrollPane(panel);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setBounds(50, 30, 300, 50);
+        splitPane.setRightComponent(scrollPane);
+        this.add(splitPane);
+
+
+        //////////////////////////////////////////////////////
 
         // frame resizing is possible
         this.setResizable(true);
@@ -55,12 +97,38 @@ public class myExplorerFrame extends JFrame {
 
         // by opening the first time the frame it will be Extended
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
         this.setJMenuBar(new navBar());
+
+
+        // makes frame to the color of the variable myBackgroundColor
+        this.getContentPane().setBackground(myBackgroundColor);
+
+        
 
         // Making the frame visible
         this.setVisible(true);
+    }
 
+}
+class myDirectory {
+    File file;
+    
+    public myDirectory(File file) {
+        this.file = file;
+    }
+
+    // get
+    public File getFile() {
+        return file;
+    }
+    // set
+    public void setFile(File file) {
+        this.file = file;
+    }
+    // change file into string
+    @Override
+    public String toString() {
+        return file.toString();
     }
 
 }
--- src/components/explorer/navBar.java
+++ src/components/explorer/navBar.java
@@ -2,6 +2,7 @@ package components.explorer;
 import java.awt.TextComponent;
 import java.awt.Dimension;
 import java.awt.ComponentOrientation;
+import java.awt.*;
 
 import javax.swing.*;  
 
@@ -9,23 +10,36 @@ public class navBar extends JMenuBar {
     /*
      * this function will display a navigation bar on top of the Explorer frame
      * {Constructor}
+     * status: not done
      */
     public navBar() {
-        JTextField t1=new JTextField("Start Searching");  
+        JTextField t1 = new JTextField();
+        JLabel label = new JLabel("   Path finder:   ");
+        Font font = new Font("Verdana", Font.PLAIN,17);
+        label.setFont(font);
         JButton btn = new JButton("Click me");
         JButton btn1 = new JButton("Click me");
         JButton btn2 = new JButton("Click me");
         JButton btn3 = new JButton("Click me");
         JButton btn4 = new JButton("Click me");
-
+        //
+        btn.setFocusPainted(false);
+        btn1.setFocusPainted(false);
+        btn2.setFocusPainted(false);
+        btn3.setFocusPainted(false);
+        btn4.setFocusPainted(false);
+        //
+        
         this.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         this.add(Box.createHorizontalGlue());
         // add menu to menu bar
         this.add(btn4);
+        this.add(label);
         this.add(t1);
         this.add(btn);
         this.add(btn1);
         this.add(btn2);
         this.add(btn3);
+        
     }
 }
